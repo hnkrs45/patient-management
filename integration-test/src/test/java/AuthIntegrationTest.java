815645829730b2520c6a02ba630d8a63d2c7e4a7
hnkrs45 <haquenawazkhan007@gmail.com>
2026-07-05 16:50:12 +0530

    Added some authentication for the login feature, but it is not working properly right now, so I will be working on it in the near future as well.

---
+++ integration-test/src/test/java/AuthIntegrationTest.java
@@ -0,0 +1,16 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class AuthIntegrationTest {
+
+    @BeforeAll
+    static void setUp(){
+        RestAssured.baseURI = "http://localhost:4004";
+    }
+
+    @Test
+    public void shouldReturnOKWithValidToken(){
+
+    }
+}
