ae805717e75c4533173db2d95ec3e91481b78c26
hnkrs45 <haquenawazkhan007@gmail.com>
2026-07-07 21:24:07 +0530

    Added some more code in Infrastructure Service, will use it later to deploy

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -1,9 +1,102 @@
 package com.pm.stack;
 import software.amazon.awscdk.Stack;
 public class LocalStack extends Stack{
+    private final Vpc vpc;
+    private final Cluster ecsCluster;
 
     public LocalStack(final App score, final String id, final StackProps props){
         super(score, id, props);
+        this.vpc=createVpc();
+
+        DatabaseInstance authServiceDb =
+                createDatabase("AuthServiceDB", "auth-service-db");
+        DatabaseInstance patientServiceDb =
+                createDatabase("PatientServiceDB", "patient-service-db");
+
+        CfnHealthCheck authDbHealthCheck =
+                createDbHealthCheck(authServiceDb, "AuthServiceDBHealthCheck");
+
+        CfnHealthCheck patientDbHealthCheck =
+                createDbHealthCheck(patientServiceDb, "PatientServiceDBHealthCheck");
+
+        CfnCLuster mskCluster = createMskCluster();
+        this.ecsCluster = createEcsCluster();
+
     }
 
+    private Vpc createVpc(){
+        return Vpc.Builder.create(this, "PatientManagementVpc")
+                .vpcName("PatientManagementVpc")
+                .maxAzs(2)
+                .build();
+    }
+
+    private DatabaseInstance createDatabase(String id, String dbName){
+        return DatabseInstanvce.Builder
+                .create(this, id)
+                .engine(DatabaseInstanceEngine.postgres(
+                        PostgresInstanceEngineProps.builder()
+                                .version(PostgresEngineVersion.VER_17_2)
+                                .build()))
+                .vpc(vpc)
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))
+                .allocatedStorage(20)
+                .credentials(Credentials.fromGeneratedSecret("admin_user"))
+                .databaseName(dbName)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+    }
+
+    private CfnHealthCheck createDbHealthCheck(DatabaseInstance db, String id){
+        return CfnHeatlthCheck.Builder.create(this, id)
+                .healthCheckConfig(CfnHealthCheck.HealthCheckConfigProperty.builder()
+                        .type("TCP")
+                        .port(Token.asNumber(db.getDbInstanceEndpointPort()))
+                        .ipAddress(db.getDbInstanceEndpointAddress())
+                        .requestInterval(30)
+                        .failureThreshold(3)
+                        .build())
+                .build();
+
+    }
+
+    private CfnCluster createMskCLuster(){
+        return CfnCLuster.Builder.create(this, "MskCLuster")
+                .clusterName("kafka-cluster")
+                .kafkaVersion("2.8.0")
+                .numberOfBrokerNodes(1)
+                .brokerNodeGroupinfo(CfnCluster.BrokerNodeGroupInfoProperty.builder()
+                        .instanceType("kafka.m5.xlarge")
+                        .clientSubnets(vpc.getPrivateSubnets().stream()
+                                .map(Isubnet::getSUbnetId)
+                                .collect(Collectors.toList()))
+                        .brokerAzDistribution("DEFAULT")
+                        .build())
+                .build();
+    }
+
+    private Cluster createEcsCluster(){
+        return Cluster.Builder.create(this, "PatientManagemantCLuster")
+                .vpc(vpc)
+                .defaultCloudMapNamespace(CloudMapNamespaceOptions.builder()
+                        .name("patient-management.local")
+                        .build())
+                .build();
+    }
+
+    private FargetService createFargetService(String id, String imageName, List<Integer> ports, DatabaseInstance db, Map<String>){
+
+    }
+
+    public static void main(final String[] args) {
+        App app = new App(AppProps.builder().outdir("./cdk.out").build());
+
+        StackProps props = StackProps.builder()
+                .synthesizer(new BootstraplessSynthesizer())
+                .build();
+
+        new LocalStack(app, "localstack", props);
+        app.synth();
+        System.out.println("App Synthesizing in Progress...");
+    }
 }
